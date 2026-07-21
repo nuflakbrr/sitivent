966d9bffd94609658ff60a4ea849ee58378d20ff
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2026-07-21 23:59:24 +0700

    hotfix: try to fix scanner

--- next.config.ts
+++ next.config.ts
@@ -58,7 +58,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            value: 'camera=(self), microphone=(), geolocation=()',
           },
         ],
       },
