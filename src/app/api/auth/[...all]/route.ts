ed0bfd34508419b529895c9981cd45e1d314a06e
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2026-07-21 22:35:07 +0700

    hotfix: try to fix better auth api

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,23 @@
-import { auth } from '@/lib/auth'; // path to your auth file
-import { toNextJsHandler } from 'better-auth/next-js';
+import { headers } from 'next/headers';
+import { auth } from '@/lib/auth';
 
-export const { POST, GET } = toNextJsHandler(auth);
+export async function POST(request: Request) {
+  const headersList = await headers();
+  const modifiedRequest = new Request(request.url, {
+    method: request.method,
+    headers: new Headers(headersList),
+    body: request.body,
+    // @ts-ignore
+    duplex: 'half',
+  });
+  return auth.handler(modifiedRequest);
+}
+
+export async function GET(request: Request) {
+  const headersList = await headers();
+  const modifiedRequest = new Request(request.url, {
+    method: request.method,
+    headers: new Headers(headersList),
+  });
+  return auth.handler(modifiedRequest);
+}
